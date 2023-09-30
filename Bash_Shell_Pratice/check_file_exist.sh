0d80f2fa6f7580e78744b83a86eadf24e4eb6223
Winsor Tse <tsewinsor@gmail.com>
2023-09-30 02:35:34 -0400

    Adding more Bash pratice

---
+++ Bash_Shell_Pratice/check_file_exist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+file="$1"
+if echo $file; then
+    echo "File is here"
+else
+    echo "File is not here"
+fi
---
+++ Bash_Shell_Pratice/create_file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#create new files and folders automatically based on parameters given
+
+
